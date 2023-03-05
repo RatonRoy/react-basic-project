e9243e203fbb76592c1cb3f1c7404054b2cb1397
RatonRoy <royraton111@gmail.com>
2023-03-06 00:32:02 +0600

    complete the according

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import './App.css';
+import Questions from './components/Questions';
+import data from './data'; 
 
 function App() {
-
+ const [questions, setQuestions ] = useState(data)
   
   return (
-    <div className="">
-      
+    <div className='container'>
+      <section className='info'>
+        {questions.map((question) => (
+          <Questions key={question.id} question={question} />
+        ))}
+      </section>
     </div>
-  );
+  )
 }
 
 export default App;
